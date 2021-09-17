36602187e12fdfbfb16dafe2977b0c300d211655
WanRicardo <wanderson.silva@gec.inatel.br>
2021-09-17 14:05:13 -0300

    First commit

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('title', 'Página contact')
+@section('content')
+<h1>Contact PAge</h1>
+@endsection
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Página home')
+
+@section('content')
+<h1>Hello World!</h1>
+@endsection
