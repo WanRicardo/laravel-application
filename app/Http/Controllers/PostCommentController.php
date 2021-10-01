e8fb86c6d333a530b91d5fce092dcbe83742ed82
WanRicardo <wanderson.silva@gec.inatel.br>
2021-10-01 16:56:21 -0300

    Queues and Background Processing

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreComment;
-use App\Mail\CommentPosted;
+use App\Jobs\NotifyUsersPostWasCommented;
+use App\Jobs\ThrottledMail;
 use App\Mail\CommentPostedMarkdown;
 use App\Models\BlogPost;
 use Illuminate\Support\Facades\Mail;
@@ -23,9 +24,26 @@ class PostCommentController extends Controller
             'user_id' => $request->user()->id,
         ]);
 
-        Mail::to($post->user)->send(
-            new CommentPostedMarkdown($comment)
-        );
+        // Mail::to($post->user)->send(
+        //     new CommentPostedMarkdown($comment)
+        // );
+
+        // Mail::to($post->user)->queue(
+        //     new CommentPostedMarkdown($comment)
+        // );
+
+        ThrottledMail::dispatch(new CommentPostedMarkdown($comment), $post->user)
+            ->onQueue('low');
+
+        NotifyUsersPostWasCommented::dispatch($comment)
+            ->onQueue('high');
+
+        // $when = now()->addMinutes(1);
+
+        // Mail::to($post->user)->later(
+        //     $when,
+        //     new CommentPostedMarkdown($comment)
+        // );
 
         return redirect()
             ->back()
